163d50e5b42bbe873caeefa1cfbdb9eb16539701
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-06-27 20:15:44 +0530

    fix

--- src/com/android/settings/fuelgauge/fastcharge/FastChargingSettings.java
+++ src/com/android/settings/fuelgauge/fastcharge/FastChargingSettings.java
@@ -187,7 +187,7 @@ public class FastChargingSettings extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.PIXEL;
+        return MetricsProto.MetricsEvent.CUSTOM;
     }
 
     /**
